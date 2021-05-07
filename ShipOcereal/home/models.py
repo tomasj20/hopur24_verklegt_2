ba46c25e9826d7c059c187b336fef134077516a6
Daníel Heiðar Tómasson <doktordanni@gmail.com>
2021-05-07 17:43:46 +0000

    Database should work

--- ShipOcereal/home/models.py
+++ ShipOcereal/home/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class cerealCategory(models.model):
+class cerealCategory(models.Model):
     name = models.CharField(max_length=255)
 
 
-class Cereal(models.model):
+class Cereal(models.Model):
     name = models.CharField(max_length=255)
     manufacturer = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
@@ -19,7 +19,7 @@ class Cereal(models.model):
     def calculate_total(self):
         self.total = self.price * ((100 - self.discount)/100)
 
-class cerealImage(models.model):
+class cerealImage(models.Model):
     image = models.CharField(max_length=9999)
     cereal = models.ForeignKey(Cereal, on_delete=models.CASCADE)
 
