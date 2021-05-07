a67e6678c510ef106e8a3d10813754a946804502
tomasj20 <tomas.eli.jafetsson@gmail.com>
2021-05-07 16:17:28 +0000

    byrjun a database tengslum

--- ShipOcereal/home/models.py
+++ ShipOcereal/home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class cerealCategory(models.model):
+    name = models.CharField(max_length=255)
+
+
+class Cereal(models.model):
+    name = models.CharField(max_length=255)
+    manufacturer = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    category = models.ForeignKey(cerealCategory, on_delete=models.CASCADE)
+    price = models.FloatField()
+    inStock = models.BooleanField()
+    onSale = models.BooleanField()
+
+class cerealImage(models.model):
+    image = models.CharField(max_length = 9999)
+    cereal = models.ForeignKey(Cereal, on_delete=models.CASCADE)
+
