eb3243d61d194b8b324b06ce7a2ae8137f79b1e3
tomasj20 <tomas.eli.jafetsson@gmail.com>
2021-05-11 19:02:41 +0000

    database

--- ShipOcereal/home/views.py
+++ ShipOcereal/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from home.models import Cereal
 import datetime
 from django.http import HttpResponse
 # Create your views here.
@@ -7,5 +8,5 @@ TEMPLATE_DIRS = (
     'os.path.join(BASE_DIR, "templates"),'
 )
 def index(request):
-    today = datetime.datetime.now().date()
-    return render(request, "home/test.html",{"today":today})
+    context = {'Cereals': Cereal.objects.all().order_by('name')}
+    return render(request, "home/test.html", context)
