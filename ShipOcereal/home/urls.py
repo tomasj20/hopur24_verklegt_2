0ff14fa35da8f9779dc9fdeb9207d42fe5a91f9a
tomasj20 <tomas.eli.jafetsson@gmail.com>
2021-05-12 12:25:37 +0000

    database fill skra aðeins breytt

--- ShipOcereal/home/urls.py
+++ ShipOcereal/home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('<int:id>', views.get_cereal_by_id(), name='cereal-details')
 ]
--- ShipOcereal/home/views.py
+++ ShipOcereal/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from home.models import Cereal
 import datetime
 from django.http import HttpResponse
@@ -10,3 +10,8 @@ TEMPLATE_DIRS = (
 def index(request):
     context = {'Cereals': Cereal.objects.all().order_by('name')}
     return render(request, "home/test.html", context)
+
+def get_cereal_by_id(request, id):
+    return render(request, 'home/cereal_details.html'), {
+        'cereal': get_object_or_404(Cereal, pk=id)
+    }
