370b566ef88daae9e7d464eb794daf2b669b2298
tomasj20 <tomas.eli.jafetsson@gmail.com>
2021-05-15 01:09:19 +0000

    gaman :)))))))) við erum búnir !!!

--- ShipOcereal/cart/urls.py
+++ ShipOcereal/cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
+    path('',views.index, name='cart-index'),
     path('cart', views.index, name='cart'),
     path('paymentstep1',views.paymentStep1,name='paymentstep1'),
     path('paymentstep2',views.paymentStep2,name='paymentstep2'),
